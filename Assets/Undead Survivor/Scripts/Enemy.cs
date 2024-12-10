bbdadda5941653a52a87bed740fd889239cf2b69
shingugitvr000 <shingugitvr000@gmail.com>
2024-12-10 23:42:16 +0900

    [Feat] 동작하게 수정

--- Assets/Undead Survivor/Scripts/Enemy.cs
+++ Assets/Undead Survivor/Scripts/Enemy.cs
@@ -7,86 +7,33 @@ public class Enemy : MonoBehaviour
     public float speed;
     public float health;
     public float maxHealth;
-    public Texture2D[] textures;  // 0-3: Run, 4: Hit, 5: Death
+    public RuntimeAnimatorController[] animCon;
     public Rigidbody2D target;
+
     public bool isLive;
 
     Rigidbody2D rigid;
     Collider2D coll;
+    Animator anim;
     SpriteRenderer spriter;
     WaitForFixedUpdate wait;
-
-    private float frameTimer = 0f;
-    private int currentRunFrame = 0;
-    private const float FRAME_RATE = 0.1f;  // 애니메이션 프레임 속도
-    private bool isHit = false;
-    private bool isDead = false;
-    private float fadeTimer = 1f;
-    private bool hasSetDeathSprite = false;  // 사망 스프라이트 설정 여부 체크
+    
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         coll = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
         wait = new WaitForFixedUpdate();
     }
 
-    private void Update()
-    {
-        if (!isLive)
-        {
-            if (isDead)
-            {
-                // 사망 스프라이트 설정
-                if (!hasSetDeathSprite)
-                {
-                    spriter.sprite = Sprite.Create(textures[5], new Rect(0, 0, textures[5].width, textures[5].height), Vector2.one * 0.5f);
-                    hasSetDeathSprite = true;
-                }
-
-                // 페이드 아웃 처리
-                fadeTimer -= Time.deltaTime;
-                Color color = spriter.color;
-                color.a = fadeTimer;
-                spriter.color = color;
-
-                if (fadeTimer <= 0)
-                {
-                    Dead();
-                }
-            }
-            return;
-        }
-
-        UpdateSprite();
-    }
-
-    void UpdateSprite()
-    {
-        frameTimer += Time.deltaTime;
-
-        if (isHit)
-        {
-            spriter.sprite = Sprite.Create(textures[4], new Rect(0, 0, textures[4].width, textures[4].height), Vector2.one * 0.5f);
-            return;
-        }
-
-        // Run animation (0-3)
-        if (frameTimer >= FRAME_RATE)
-        {
-            frameTimer = 0f;
-            currentRunFrame = (currentRunFrame + 1) % 4;  // 0부터 3까지 순환
-            Texture2D currentTexture = textures[currentRunFrame];
-            spriter.sprite = Sprite.Create(currentTexture, new Rect(0, 0, currentTexture.width, currentTexture.height), Vector2.one * 0.5f);
-        }
-    }
 
     private void FixedUpdate()
     {
         if (!GameManager.instance.isLive)
             return;
-        if (!isLive || isHit)
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
             return;
 
         Vector2 dirVec = target.position - rigid.position;
@@ -109,20 +56,13 @@ public class Enemy : MonoBehaviour
         coll.enabled = true;
         rigid.simulated = true;
         spriter.sortingOrder = 2;
-        isDead = false;
-        isHit = false;
-        hasSetDeathSprite = false;
+        anim.SetBool("Dead", false);
         health = maxHealth;
-        fadeTimer = 1f;
-
-        // 스프라이트 알파값 초기화
-        Color color = spriter.color;
-        color.a = 1f;
-        spriter.color = color;
     }
 
     public void Init(SpawnData data)
     {
+        anim.runtimeAnimatorController = animCon[data.spriteType];
         speed = data.speed;
         maxHealth = data.health;
         health = data.health;
@@ -132,46 +72,45 @@ public class Enemy : MonoBehaviour
     {
         if (!collision.CompareTag("Bullet"))
             return;
-
         health -= collision.GetComponent<Bullet>().damage;
+
+        //StartCoroutine("KnockBack");
         StartCoroutine(KnockBack());
-        StartCoroutine(HitEffect());
 
         if (health > 0)
         {
+            anim.SetTrigger("Hit");
             AudioManager.instance.PlaySfx(AudioManager.Sfx.Hit);
         }
         else
         {
             isLive = false;
-            isDead = true;
             coll.enabled = false;
             rigid.simulated = false;
             spriter.sortingOrder = 1;
+            anim.SetBool("Dead",true);
             GameManager.instance.kill++;
             GameManager.instance.GetExp();
-            if (GameManager.instance.isLive)
+
+            if(GameManager.instance.isLive)
                 AudioManager.instance.PlaySfx(AudioManager.Sfx.Dead);
         }
     }
 
-    IEnumerator HitEffect()
-    {
-        isHit = true;
-        yield return new WaitForSeconds(0.2f);  // 히트 스프라이트 지속 시간
-        isHit = false;
-    }
-
     IEnumerator KnockBack()
     {
-        yield return wait;
+        //yield return null;  // 1프레임 쉬기
+        //yield return new WaitForSeconds(2f);    // 2초 쉬기
+        yield return wait;//하나의 물리 프레임을 딜레이 주기
         Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 dirVec = transform.position - playerPos;
-        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+        rigid.AddForce(dirVec.normalized * 3,ForceMode2D.Impulse);
+
     }
 
     void Dead()
     {
         gameObject.SetActive(false);
     }
+
 }
